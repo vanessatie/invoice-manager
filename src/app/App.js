be23294c46145d47c1cc5e22cc98a80c059f5864
Vanessa Tiedemann <vanessa.tiedemann@hotmail.com>
2019-07-26 10:45:16 +0200

    add globalstyles

---
+++ src/app/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import GlobalStyles from "./GlobalStyles";
+
+function App() {
+  return (
+    <>
+      <GlobalStyles /> <div>Test</div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/app/GlobalStyles.js
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+* {
+    box-sizing: border-box;
+    }
+html, body {
+    margin: 0;
+}
+body {
+    font-family: sans-serif;
+}
+`;
