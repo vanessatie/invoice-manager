2f35a2f8fa587741074174fa925340dfc14ea300
Vanessa Tiedemann <vanessa.tiedemann@hotmail.com>
2019-08-07 18:56:25 +0200

    add filter to list

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -3,6 +3,7 @@ import PropTypes from "prop-types";
 import Card from "./Card";
 
 function CardList({ cards, onCardClick }) {
+  const [month, setMonth] = React.useState(defaultMonth());
   function renderCard(card) {
     return <Card key={card._id} {...card} onClick={() => onCardClick(card)} />;
   }
@@ -19,12 +20,53 @@ function CardList({ cards, onCardClick }) {
     return 0;
   }
 
+  function defaultMonth() {
+    let today = new Date();
+    let currentMonth = today.getMonth() + 1;
+
+    return currentMonth;
+  }
+  function filterMonth(element) {
+    if (month < 10) {
+      return element.date.includes("-0" + month + "-");
+    }
+    if (month < 9) {
+      return element.date.includes("-" + month + "-");
+    }
+  }
+
+  function handleMonthChange(event) {
+    setMonth(event.target.value);
+  }
+
   return (
     <>
-      {cards
-        .slice()
-        .sort(sortDate)
-        .map(renderCard)}
+      <select name="month" onChange={event => handleMonthChange(event)}>
+        <option value="">Select</option>
+        <option value="1">Januar</option>
+        <option value="2">Februar</option>
+        <option value="3">März</option>
+        <option value="4">April</option>
+        <option value="5">Mai</option>
+        <option value="6">Juni</option>
+        <option value="7">Juli</option>
+        <option value="8">August</option>
+        <option value="9">September</option>
+        <option value="10">Oktober</option>
+        <option value="11">November</option>
+        <option value="12">Dezember</option>
+      </select>
+
+      {month === ""
+        ? cards
+            .slice()
+            .sort(sortDate)
+            .map(renderCard)
+        : cards
+            .filter(filterMonth)
+            .slice()
+            .sort(sortDate)
+            .map(renderCard)}
     </>
   );
 }
